dbe3c8113298aa6c38aac49884a05e8997c89f28
MrEkwunife <ekwunifechimobi16@gmail.com>
2026-01-16 10:24:54 +0100

    reset

--- src/command_exit.ts
+++
@@ -1,7 +0,0 @@
-import { State } from "./state";
-
-export async function commandExit(state: State) {
-  console.log("Closing the Pokedex... Goodbye!");
-  state.pokeAPI.closeCache();
-  process.exit(0);
-}
--- src/main.ts
+++
@@ -1,9 +0,0 @@
-import { startREPL } from "./repl.js";
-import { initState } from "./state.js";
-
-function main() {
-  const state = initState(1000 * 60 * 60);
-  startREPL(state);
-}
-
-main();
